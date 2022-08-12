5dca4ed0d964b0ef260f6fb7d7a1556980ed5d38
Sergei Sheshko <Sheshko_SS@mail.ru>
2022-08-12 15:34:14 +0300

    second homework rep

--- app/src/main/java/com/example/androidcalculator/MainActivity.kt
+++ app/src/main/java/com/example/androidcalculator/MainActivity.kt
@@ -3,9 +3,32 @@ package com.example.androidcalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+
+/*
+Исползую алгоритм обратной польской нотации (RPN), нашёл его на Вики,
+и пару примеров реализации на GitHub, это то что получилось у меня.
+Тут не предусмотрен ввод десятичных чисел и отрицательных чисел вида -2*3,
+но (0-2)*3 работать будет
+*/
+
+/*
+Добавил ещё историю в виде списка прошлых мат операций, сделал старым способом, в будущем попробую
+через ListAdapter сделать
+*/
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //компонент, содержащий список всех врагментов на activity
+        supportFragmentManager
+            .beginTransaction()
+            //ради одной строки binding не вводил, использую его в GUIFragment
+            //добавляем наш фрагмент
+            .add(R.id.g_u_i_container, GUIFragment())
+            .commit()
+
     }
 }
